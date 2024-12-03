a5258a9ae6971257903c30163203cbb03056a034
Shaoyu Wang <shaoyuwang119@gmail.com>
2024-12-03 13:38:14 -0600

    Create Day3a.java

---
+++ Day3a.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scan = new Scanner(new File("input.txt"));
+        File f = new File("input.txt");
+        String file = "";
+        int sum = 0;
+        
+        
+        while (scan.hasNextLine()) {
+            file += scan.nextLine();
+        }
+        
+        for (int i=0; i<file.length()-1; i++) {
+    	    if (file.substring(i+1,i+2).equals("u") && file.substring(i+2,i+3).equals("l") && file.substring(i+3,i+4).equals("(")) {
+    	        System.out.println(file.substring(i, i+12));
+    	        String sx = "";
+    	        String sy = "";
+    	        int x = 0;
+    	        int y = 0;
+    	        int j = i+4;
+    	        
+    	        while (!file.substring(j,j+1).equals(",")) {
+    	            sx += file.substring(j,j+1);
+    	            j++;
+    	            System.out.println(sx);
+    	        }
+    	        
+    	        try {
+    	            x = Integer.parseInt(sx);
+    	        } catch (NumberFormatException e) {
+    	            System.out.println("false");
+    	            continue;
+    	            
+    	        }
+    	        
+    	        j++;
+    	        
+    	        while (!file.substring(j,j+1).equals(")")) {
+    	            sy += file.substring(j,j+1);
+    	            j++;
+    	        }
+    	        try {
+    	            y = Integer.parseInt(sy);
+    	        } catch (NumberFormatException e) {
+    	            System.out.println("false");
+    	            continue;
+    	        }
+    	        
+    	        System.out.println(x*y);
+    	        sum += x*y;
+    	        
+    	    }
+    	
+        }
+		    System.out.println(sum);
+	}
+	
+	    
+}
