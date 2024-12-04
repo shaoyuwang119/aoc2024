8dfd7f602eab239c177322be5a436f1206a33c8d
Shaoyu Wang <shaoyuwang119@gmail.com>
2024-12-04 00:46:29 -0600

    Update Day4a.java

--- Day4a.java
+++ Day4a.java
@@ -4,8 +4,7 @@ import java.io.*;
 public class Day4a
 {
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner scan = new Scanner(new File("input.txt"));
-        File f = new File("tests.txt");
+	Scanner scan = new Scanner(new File("input.txt"));
         
         // initalize array
         scan.useDelimiter("");
